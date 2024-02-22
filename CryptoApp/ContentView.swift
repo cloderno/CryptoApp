fb3db98eb4568ed5ef1016571f9aaf5290cf4dc0
cloderno <cloderno@gmail.com>
2024-02-22 20:21:13 +0600

    Initial Commit

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Rating on 22.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Rating on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
